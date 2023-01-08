cf6312bfd85ad6ae9d31507520ef3a87811676a9
lengocson <leson131002@gmail.com>
2023-01-09 00:29:09 +0700

    Fix ngu

--- src/main/java/com/swd/bike/TestController.java
+++ src/main/java/com/swd/bike/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Value("${test.messsage}")
+    @Value("${test.message}")
     private String message;
 
     @Value("${test.secret}")
