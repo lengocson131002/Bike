826a542fab1b9c429a00ddea79b57ff5c297e487
lengocson <leson131002@gmail.com>
2023-01-09 00:02:42 +0700

    Fix ci

--- src/main/java/com/swd/bike/TestController.java
+++ src/main/java/com/swd/bike/TestController.java
@@ -1,11 +1,27 @@
 package com.swd.bike;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${test.messsage}")
+    private String message;
+
+    @Value("${test.secret}")
+    private String secret;
+
+    @GetMapping("/message")
+    public String message() {
+        return message;
+    }
+
+    @GetMapping("/secret")
+    public String secret() {
+        return secret;
+    }
 
     @GetMapping("/test")
     public String test() {
