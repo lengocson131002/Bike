f20444a04cc424018d78666d2b561e605c409d33
Nguyễn Trung Tín <se.nttin1811@gmail.com>
2023-02-04 13:47:32 +0000

    Fix/catch exception

--- src/main/java/com/swd/bike/enums/ResponseCode.java
+++ src/main/java/com/swd/bike/enums/ResponseCode.java
@@ -8,16 +8,16 @@ public enum ResponseCode {
     INVALID_PARAM(3, "Invalid param"),
     INVALID_SESSION(4, "Invalid session"),
     UNHANDLED_REQUEST(5, "Unhandled request"),
-    THIRD_PARTY_KEYCLOAK_ERROR(6, "Error when calling third party [Keycloak]"),
+    THIRD_PARTY_ERROR(6, "Error when calling third party"),
     JSON_PROCESSING_ERROR(7, "Error when processing JSON"),
 
     // Auth
     INVALID_USERNAME_OR_PASSWORD(10, "Invalid username or password"),
     GOOGLE_AUTH_ERROR(11, "Error occurs when login with Google"),
     REFRESH_TOKEN_INVALID(12, "Invalid refresh token"),
-    AUTHENTICATION_FAILED(13,"Authentication failed"),
-    AUTHENTICATION_FAILED_OUTSIDE_EMAIL(14,"Authentication failed: Outside email"),
-    CODE_INVALID(15,"Invalid code"),
+    AUTHENTICATION_FAILED(13, "Authentication failed"),
+    AUTHENTICATION_FAILED_OUTSIDE_EMAIL(14, "Authentication failed: Outside email"),
+    CODE_INVALID(15, "Invalid code"),
 
     // User
     USER_NOT_FOUND(20, "User not found"),
@@ -41,4 +41,4 @@ public enum ResponseCode {
     public String getMessage() {
         return message;
     }
-    }
+}
