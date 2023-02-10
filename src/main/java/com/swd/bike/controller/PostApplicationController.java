8edaca1d5f1ac24d1726b6e451c1e249876ee655
lengocson <leson131002@gmail.com>
2023-02-10 23:12:16 +0700

    Done get all applications API

--- src/main/java/com/swd/bike/controller/PostApplicationController.java
+++ src/main/java/com/swd/bike/controller/PostApplicationController.java
@@ -4,8 +4,8 @@ import com.swd.bike.controller.interfaces.IPostApplicationController;
 import com.swd.bike.core.BaseController;
 import com.swd.bike.core.ResponseBase;
 import com.swd.bike.dto.application.request.GetAllApplicationsRequest;
+import com.swd.bike.dto.application.response.AppliedPostResponse;
 import com.swd.bike.dto.common.PageResponse;
-import com.swd.bike.dto.userPost.response.PostResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostApplicationController extends BaseController implements IPostApplicationController {
 
     @Override
-    public ResponseEntity<ResponseBase<PageResponse<PostResponse>>> getAllApplyingPost(GetAllApplicationsRequest request) {
+    public ResponseEntity<ResponseBase<PageResponse<AppliedPostResponse>>> getAllApplyingPost(GetAllApplicationsRequest request) {
         return this.execute(request);
     }
 }
